1afe4e1c08f545700068f2b465591b40e682fb7a
KeviN-DeigntoN <lezhibokov.vladimir@yandex.ru>
2020-11-03 11:10:23 +0300

    1 задачка

--- LVR_Task_01/LVR_Task_01/Program.cs
+++ LVR_Task_01/LVR_Task_01/Program.cs
@@ -5,18 +5,30 @@ namespace LVR_Task_01
     class Program
     {
         //Стоимость 1 кристалла
-        public const int Quality = 100;
+        private const int _quality = 10;
         static void Main(string[] args)
         {
+            
             Console.WriteLine("Введите кол-во золота: ");
-            int Gold = Convert.ToInt32(Console.ReadLine());
+            int gold = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Сколько кристаллов вы хотите приобрести? Курс: 1 кристалл = {Quality} золота");
+            Console.WriteLine($"Сколько кристаллов вы хотите приобрести? Курс: 1 кристалл = {_quality} золота");
             int crystals = Convert.ToInt32(Console.ReadLine());
 
+            while(crystals*_quality > gold)
+            {
+                Console.WriteLine("У вас недостаточное количество золота");
+                crystals = 0;
+                break;
+            }
 
+            while (crystals * _quality < gold)
+            {
+                gold -= crystals * _quality;
+                break;
+            }
 
-
+            Console.WriteLine($"У вас {gold} золота и {crystals} кристаллов. Удачи!");
         }
     }
 }
