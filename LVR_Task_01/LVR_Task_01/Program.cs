ce8accae74a0ba280a900099a81c88440ebb0c7f
KeviN-DeigntoN <lezhibokov.vladimir@yandex.ru>
2020-11-03 10:44:49 +0300

    Задания

---
+++ LVR_Task_01/LVR_Task_01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LVR_Task_01
+{
+    class Program
+    {
+        //Стоимость 1 кристалла
+        public const int Quality = 100;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите кол-во золота: ");
+            int Gold = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Сколько кристаллов вы хотите приобрести? Курс: 1 кристалл = {Quality} золота");
+            int crystals = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+        }
+    }
+}
---
+++ LVR_Task_01/LVR_Task_02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LVR_Task_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
