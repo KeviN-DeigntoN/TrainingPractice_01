151b011ef464567af4405ae4e1434682557aae79
KeviN-DeigntoN <lezhibokov.vladimir@yandex.ru>
2020-11-05 09:47:29 +0300

    фикс

--- LVR_Task_01/LVR_Task_04/Program.cs
+++ LVR_Task_01/LVR_Task_04/Program.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.Design;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace LVR_Task_04
 {
