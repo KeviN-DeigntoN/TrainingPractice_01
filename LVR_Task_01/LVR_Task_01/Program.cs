9b36ec1bea06af680c0f3a4f376f84a31ef3a0a1
KeviN-DeigntoN <lezhibokov.vladimir@yandex.ru>
2020-11-06 10:09:26 +0300

    Фикс 1 задачи

--- LVR_Task_01/LVR_Task_01/Program.cs
+++ LVR_Task_01/LVR_Task_01/Program.cs
@@ -15,17 +15,14 @@ namespace LVR_Task_01
             Console.WriteLine($"Сколько кристаллов вы хотите приобрести? Курс: 1 кристалл = {_quality} золота");
             int crystals = Convert.ToInt32(Console.ReadLine());
 
-            while(crystals*_quality > gold)
+            if (crystals*_quality > gold)
             {
                 Console.WriteLine("У вас недостаточное количество золота");
                 crystals = 0;
-                break;
             }
-
-            while (crystals * _quality < gold)
+            else
             {
                 gold -= crystals * _quality;
-                break;
             }
 
             Console.WriteLine($"У вас {gold} золота и {crystals} кристаллов. Удачи!");
