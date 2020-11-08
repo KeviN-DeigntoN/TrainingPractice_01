ff258d1bc5dd4c24e6251c2f32bf63726fc5122a
KeviN-DeigntoN <lezhibokov.vladimir@yandex.ru>
2020-11-08 13:49:37 +0300

    .

--- LVR_Task_01/LVR_Task_06/Program.cs
+++ LVR_Task_01/LVR_Task_06/Program.cs
@@ -1,20 +1,21 @@
 ﻿using System;
-
+using System.Diagnostics;
+using System.Reflection;
 namespace LVR_Task_06
 {
     class Program
     {
         static string[,] _users = new string[0, 2];
         static int _size = 0;
-
-        static void Push(string surname, string post)
+        //Запись
+        static void Record(string surname, string post)
         {
-            Resize(_size + 1);
+            Resize((_size + 1));
             _users[_size - 1, 0] = surname;
             _users[_size - 1, 1] = post;
-            Console.WriteLine($"В базу добавлен {surname} с должностью {post}, номер: {_size}");
+            Console.WriteLine($"В базу добавлен {surname}, должность - {post}, номер: {_size}");
         }
-
+        //Увеличение для записи
         static void Resize(int newsize)
         {
             if (newsize < 0)
@@ -39,13 +40,13 @@ namespace LVR_Task_06
                 _size = newsize;
             }
         }
-
+        //Вывод
         static void List()
         {
             if (_size == 0) Console.WriteLine("Нет досье, вначале добавьте кого-нибудь...");
             else for (int i = 0; i < _size; i++) Console.WriteLine($"{i + 1}) {_users[i, 0]} - {_users[i, 1]}");
         }
-
+        //Удаление
         static void Remove(int pos)
         {
             if (pos >= _size || pos < 0 || _size <= 0)
@@ -73,7 +74,7 @@ namespace LVR_Task_06
             _users = newer;
             _size -= 1;
         }
-
+        //Поиск
         static void Search(string str)
         {
             int count = 0;
@@ -85,23 +86,21 @@ namespace LVR_Task_06
                     count++;
                 }
             }
-            if (count == 0) Console.WriteLine("Не нашлось досье с такими фамилиями");
+            if (count == 0) Console.WriteLine("Таких досье нет.");
         }
 
-        static void Info()
-        {
-            Console.WriteLine("Отдел кадров..\n1. Добавить досье \n2. Вывести все досье \n3. Удалить досье \n4. Поиск по фамилии \n5. Выйти из программы\n6. Показать эту справку");
-        }
+      
 
         static void Main(string[] args)
         {
-
-            Info();
+            //Главная информация
+            Console.WriteLine("Отдел кадров..\n1. Добавить досье \n2. Вывести все досье \n3. Удалить досье \n4. Поиск по фамилии \n5. Выйти из программы\n6. Показать эту справку");
+            //Действия
             while (true)
             {
-                Console.WriteLine("\nОжидание ввода...\n------------------------------\n");
-                char key = Console.ReadKey(true).KeyChar;
-                if (key == '1')
+                Console.WriteLine("\nОжидание ввода...\n");
+                string select = Console.ReadLine();
+                if (select == "1")
                 {
                     Console.WriteLine("Введите фамилию: ");
                     string surname = Console.ReadLine();
@@ -111,13 +110,13 @@ namespace LVR_Task_06
                     string patronymic = Console.ReadLine();
                     Console.WriteLine("Введите должность: ");
                     string post = Console.ReadLine();
-                    Push(surname + " " + name + " " + patronymic, post);
+                    Record(surname + " " + name + " " + patronymic, post);
                 }
-                else if (key == '2')
+                else if (select == "2")
                 {
                     List();
                 }
-                else if (key == '3')
+                else if (select == "3")
                 {
                     if (_size < 1) Console.WriteLine("В базе нет досье.");
                     else
@@ -127,24 +126,20 @@ namespace LVR_Task_06
                         Remove(pos - 1);
                     }
                 }
-                else if (key == '4')
+                else if (select == "4")
                 {
                     Console.WriteLine("Введите фамилию для поиска: ");
                     string surname = Console.ReadLine();
                     Search(surname);
                 }
-                else if (key == '5')
+                else if (select == "5")
                 {
-                    Console.WriteLine("Завершение работы программы..");
+                    Console.WriteLine("Завершение работы программы...");
                     System.Environment.Exit(1);
-                }
-                else if (key == '6')
-                {
-                    Info();
                 }
                 else
                 {
-                    Console.WriteLine("Пожалуйста, выберите что-то от 1 до 6.");
+                    Console.WriteLine("Введите значение от 1 до 5.");
                 }
             }
         }
---
+++ LVR_Task_01/LVR_Task_07/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LVR_Task_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+            {
+                Random rnd = new Random();
+                Console.WriteLine("Размер массива: ");
+                int size = Convert.ToInt32(Console.ReadLine());
+                int[] arr = new int[size];
+
+                for (int i = 0; i < size; i++) arr[i] = rnd.Next(0, 100);
+
+                Console.WriteLine($"Было:");
+                Console.WriteLine(string.Join(" ", arr));
+
+                Shuffle(size, arr);
+
+                Console.WriteLine($"Стало: ");
+                Console.WriteLine(string.Join(" ", arr));
+
+            }
+        static void Shuffle(int size, int[] arr)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                int rndI2 = rnd.Next(0, i);
+                int rndI1 = rnd.Next(0, i + 1);
+                int temp = arr[rndI2];
+                arr[rndI2] = arr[rndI1];
+                arr[rndI1] = temp;
+            }
+        }
+    }
+}
